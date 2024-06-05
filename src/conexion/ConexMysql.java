3ef8d523c12788754f463f7a67ca65e95b1a53b3
Roberto Ivan Aguirre Silerio <ext_riaguirres@falabella.com.mx>
2024-06-05 11:57:34 -0600

    Mejora a conexion de base de datos

--- src/conexion/ConexMysql.java
+++ src/conexion/ConexMysql.java
@@ -1,33 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-/**
- *
- * @author jhonf
- */
+ 
 public class ConexMysql {
-   Connection conectar = null;
 
-    public Connection establecerConexion(){
+    Connection conectar = null;
+    DB db = new DB("localhost", "3306", "mknicar", "root", "Ilike2pizza@");
+    String cadenaConexion = "jdbc:mysql://"+db.construirCadena();
+    
+
+    public Connection establecerConexion() {
+    System.out.println(cadenaConexion);
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mknicar","root","user123");
-            System.out.println("CONECTADO"); 
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mknicar", db.getUsuario(), db.getContraseña());
+            System.out.println("CONECTADO");
             return cn;
-            
+
+        } catch (Exception e) {
+            System.out.println("ERROR DE CONEXION : " + e);
+            return null;
         }
-        catch (Exception e) {
-             System.out.println("ERROR DE CONEXION : "+e);
-        return null;
-        }
+
     }
-    
-    
+
 }
---
+++ src/conexion/DB.java
@@ -0,0 +1,43 @@
+package conexion;
+
+public class DB {
+
+    private String ip;
+    private String puerto;
+    private String baseDatos;
+    private String usuario;
+    private String contraseña;
+    public String Driver = "com.mysql.jdbc.Driver";
+
+    public DB(String ip, String puerto, String baseDatos, String usuario, String contraseña) {
+        this.ip = ip;
+        this.puerto = puerto;
+        this.baseDatos = baseDatos;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public String getPuerto() {
+        return puerto;
+    }
+
+    public String getBaseDatos() {
+        return baseDatos;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public String construirCadena() {
+        return this.ip + ":" + this.puerto + "/"+this.baseDatos+"?autoReconnect=true&useSSL=false";
+    }
+}
